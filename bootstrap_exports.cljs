e123d59aa50290139ffaad9d2f1a97359611dce2
Matt Huebert <mhuebert@gmail.com>
2016-11-07 20:20:46 +0100

    Delete bootstrap_exports.cljs

--- bootstrap_exports.cljs
+++
@@ -1,5 +0,0 @@
-{:in-path  "./resources/public/js/compiled/out"
- :out-path "./resources/public/js/compiled"
- :caches   #{[firelight.dev :cljs]}
- :macros   #{[firelisp.ruleset :cljc]
-             [firelisp.db :cljc]}}
