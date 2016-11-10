a51bdfb4522c888a804c9e80e4b2bbd5ad8dd781
Matthew Huebert <no-email@gmail.com>
2016-11-10 17:27:49 +0100

    Fix closure dep order

--- script/read_closure_library_deps.js
+++ script/read_closure_library_deps.js
@@ -1,5 +1,3 @@
 require("google-closure-library");
 require("google-closure-library/closure/goog/deps.js");
-fs = require("fs")
-fs.writeFileSync(__dirname + "/deps_cache.json", JSON.stringify(goog.dependencies_));
 console.log(JSON.stringify(goog.dependencies_));
