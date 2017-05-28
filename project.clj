25af2c0cc22a9d67b58b8df56b4412a539e02e40
Matthew Huebert <no-email@gmail.com>
2017-05-28 17:18:09 +0200

    lein-release plugin: preparing 0.1.15 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-live "0.1.15-SNAPSHOT"
+(defproject cljs-live "0.1.15"
   :description "Tools for bunding dependencies for self-hosted ClojureScript"
   :url "https://github.com/mhuebert/cljs-live"
   :license {:name "Eclipse Public License"
