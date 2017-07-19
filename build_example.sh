b57760559c90742be41e525d4fa264c6e7073893
Matthew Huebert <no-email@gmail.com>
2017-07-19 17:35:20 +0200

    Update example script

--- build_example.sh
+++ build_example.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 java -cp ~/cljs.jar:src:`lein with-profile dev classpath` clojure.main script/build_example.clj $@;
-./bundle.sh live-deps.clj;
+
+# TODO: update this with new instructions
+#./bundle.sh live-deps.clj;
