9e3142c4c51da76ba1239e7572b9e2674482d2ba
Matthew Huebert <no-email@gmail.com>
2017-07-25 12:03:11 +0200

    lein-release plugin: bumped version from 0.2.2 to 0.2.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-live "0.2.2"
+(defproject cljs-live "0.2.3-SNAPSHOT"
   :description "Tools for bunding dependencies for self-hosted ClojureScript"
   :url "https://github.com/mhuebert/cljs-live"
   :license {:name "Eclipse Public License"
