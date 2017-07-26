854a194db86b349e89a125b141cf9b2e3a150960
Matthew Huebert <no-email@gmail.com>
2017-07-26 11:58:59 +0200

    lein-release plugin: preparing 0.2.3 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljs-live "0.2.3-SNAPSHOT"
+(defproject cljs-live "0.2.3"
   :description "Tools for bunding dependencies for self-hosted ClojureScript"
-  :url "https://github.com/mhuebert/cljs-live"
+  :url "https://github.com/braintripping/cljs-live"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.7.1"
@@ -23,3 +23,6 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled/out" ".planck_cache"]
 
   :source-paths ["src" "script"])
+
+
+
